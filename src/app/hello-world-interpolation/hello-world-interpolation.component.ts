1933d4e4656f505977379a5eea241dde9b95b6f3
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2021-03-12 09:51:32 +0100

    Use the HTML Like template inside the component

--- src/app/hello-world-interpolation/hello-world-interpolation.component.ts
+++ src/app/hello-world-interpolation/hello-world-interpolation.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'hello-world-interpolation',
-  templateUrl: './hello-world-interpolation.component.html'
+  // templateUrl: './hello-world-interpolation.component.html',
+  template: `<p>{{message}} 2</p>`
 })
 export class HelloWorldInterpolationComponent {
     message = 'Hello, World!';
